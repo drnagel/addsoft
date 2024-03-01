e2106c8d00be23dd88b6655b6a2853ab4c997176
Demian Nagel <drnagel@gmail.com>
2024-03-01 19:11:48 +0000

    Added applications to be installed.

---
+++ setup-start.sh
@@ -0,0 +1,24 @@
+installFirefox(){
+    
+    flatpak install flathub org.mozilla.firefox -y
+}
+
+installLibreOffice(){
+    
+    flatpak install flathub org.libreoffice.LibreOffice -y
+}
+
+installVSCode(){
+    
+    flatpak install flathub com.visualstudio.code -y
+}
+
+installDiscord(){
+    
+    flatpak install flathub com.discordapp.Discord -y
+}
+
+installFirefox
+installLibreOffice
+installVSCode
+installDiscord
---
+++ setup-video.sh
@@ -0,0 +1,18 @@
+installKdenLive(){
+    
+    flatpak install flathub org.kde.kdenlive -y
+}
+
+installHandBrake(){
+    
+    flatpak install flathub fr.handbrake.ghb -y
+}
+
+installLossLessCut(){
+    
+    flatpak install flathub no.mifi.losslesscut -y
+}
+
+installKdenLive
+installHandBrake
+installLossLessCut
